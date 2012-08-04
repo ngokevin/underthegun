fbbdff115ee6b848e6746ef2357dae7eeff6f9fc
Kevin Ngo <ktngo09@gmail.com>
2012-08-03 23:48:08 -0700

    commented the matchmaking system

--- client/js/matchmaking.js
+++ client/js/matchmaking.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
     var playerId;
 
+    // Connect to the match-making system.
     $('#find-match').click(function() {
         var socket = io.connect('http://localhost:3479');
 
+        // Server will tell us what our player id is if we don't have one.
         socket.on('assign-player-id', function(data) {
             playerId = data.playerId;
             console.log('Your player id is ' + playerId);
         });
 
+        // Match found, start a game.
         socket.on('match-found', function(data) {
-            console.log('Player found (' + data.playerId + '), start game.');
+            console.log('Player found (' + data.matchId + '), start game.');
         });
 
+        // Match not found so keep polling until a match is found.
         socket.on('find-match-retry', function() {
             console.log('No match found, retrying.');
             setTimeout(function() { socket.emit('find-match-retry', { playerId: playerId }); }, 3000);
--- server/underthegun.js
+++ server/underthegun.js
@@ -17,23 +17,14 @@ new mysql.Database({
     }
 });
 
-var io = require('socket.io').listen(http);
-function f() { return false; }
+// Global vars.
+var numPlayers = 0, waitingPlayers = [], matchFoundQueue = {};
 
-var numPlayers = 0, waitingPlayers = [];
+var io = require('socket.io').listen(http);
 io.sockets.on('connection', function(socket) {
     var playerId;
 
-    function findMatch(playerId, retrying) {
-        // Pop player if there's a player waiting.
-        if (!retrying && waitingPlayers.length > 0) {
-            return socket.emit('match-found', { playerId: waitingPlayers.shift() });
-        } else if (!retrying) {
-            waitingPlayers.push(playerId);
-        }
-        socket.emit('find-match-retry');
-    }
-
+    // Try to find a match, and add to waiting list if can't find one.
     socket.on('find-match', function(data) {
         // Assign a player id to player if they don't have one.
         if (!('playerId' in data)) {
@@ -46,14 +37,36 @@ io.sockets.on('connection', function(socket) {
         findMatch(playerId, false);
     });
 
+    // Find a match knowing the player is already in the waiting list.
     socket.on('find-match-retry', function(data) {
         findMatch(data.playerId, true);
     });
 
+    // Remove player from waiting list if disconnected.
     socket.on('disconnect', function() {
         var index = waitingPlayers.indexOf(playerId);
         waitingPlayers.splice(index, index);
     });
+
+    function findMatch(playerId, retrying) {
+        // Match a player if there's already player waiting.
+        if (!retrying && waitingPlayers.length > 0) {
+            var matchId = waitingPlayers.shift();
+            matchFoundQueue[matchId] = playerId;
+            return socket.emit('match-found', { matchId: matchId });
+        // Else, join the waiting list and retry.
+        } else if (!retrying) {
+            waitingPlayers.push(playerId);
+        // If retrying, check if someone matched up with the player while the
+        // player was waiting.
+        } else {
+            if (playerId in matchFoundQueue) {
+                return socket.emit('match-found', { matchId: matchFoundQueue[playerId] });
+            }
+        }
+        socket.emit('find-match-retry');
+    }
 });
 
+function f() { return false; }
 console.log('Server running at localhost:3479');
