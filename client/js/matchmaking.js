851a64d62db088bd3fac98bd6f31dfeb954de8d7
Kevin Ngo <ktngo09@gmail.com>
2012-08-04 03:04:29 -0700

    crappy landing design

--- client/js/matchmaking.js
+++ client/js/matchmaking.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     var playerId;
 
+    $('.button').mousedown(function() { $(this).addClass('clicked'); });
+    $('.button').mouseup(function() { $(this).removeClass('clicked'); });
+
     // Connect to the match-making system.
     $('#find-match').click(function() {
         var socket = io.connect('http://localhost:3479');
@@ -14,16 +17,30 @@ $(document).ready(function() {
         // Match found, start a game.
         socket.on('match-found', function(data) {
             console.log('Player found (' + data.matchId + '), start game.');
+            game();
         });
 
         // Match not found so keep polling until a match is found.
         socket.on('find-match-retry', function() {
-            console.log('No match found, retrying.');
             setTimeout(function() { socket.emit('find-match-retry', { playerId: playerId }); }, 3000);
         });
 
         socket.emit('find-match', { playerId: playerId });
         console.log('Looking for match...');
     });
+
+
+    function game() {
+        $('#lobby').hide();
+        $('#game').show();
+
+        socket.on('new-game', function(data) {
+            console.log('Starting new game.');
+
+            socket.on('new-hand', function(data) {
+                console.log('Starting new hand.');
+            });
+        });
+    }
 
 });
