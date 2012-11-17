30bb992d6c0d72d976c6fec65138f6afc3d35b13
Kevin Ngo <ktngo09@gmail.com>
2012-11-17 02:51:59 -0800

    more tests

--- server/tests/test_holdem.js
+++ server/tests/test_holdem.js
@@ -103,9 +103,110 @@ var testCompareHands = {
     },
 
     testHighCard: function(test) {
+        var handA = createHand(['Ac', 'Td', '7h', '5s', '2c'], true);
+        var handB = createHand(['As', 'Th', '7d', '5c', '3s'], true);
+        test.equal(h.compareHands(handA, handB), -1);
+
+        var handA = createHand(['Ac', 'Td', '7h', '5s', '3c'], true);
+        var handB = createHand(['As', 'Th', '7d', '5c', '3s'], true);
+        test.equal(h.compareHands(handA, handB), 0);
+
         var handA = createHand(['Ac', 'Td', '7h', '5s', '3c'], true);
         var handB = createHand(['As', '9h', '7d', '5c', '3s'], true);
         test.equal(h.compareHands(handA, handB), 1);
+
+        test.done()
+    },
+
+    testPair: function(test) {
+        var handA = createHand(['Ac', 'Ad', '7h', '5s', '2c'], true);
+        var handB = createHand(['As', 'Ah', '7d', '5c', '3s'], true);
+        test.equal(h.compareHands(handA, handB), -1);
+
+        var handA = createHand(['Kc', 'Kd', '7h', '5s', '2c'], true);
+        var handB = createHand(['Ks', 'Kh', '7d', '5c', '2s'], true);
+        test.equal(h.compareHands(handA, handB), 0);
+
+        var handA = createHand(['Ac', 'Ad', '7h', '5s', '2c'], true);
+        var handB = createHand(['Js', 'Jh', '7d', '5c', '3s'], true);
+        test.equal(h.compareHands(handA, handB), 1);
+
+        test.done()
+    },
+
+    testTwoPair: function(test) {
+        var handA = createHand(['Tc', 'Td', '5h', '5s', 'Ac'], true);
+        var handB = createHand(['Js', 'Jh', '3d', '3c', '2s'], true);
+        test.equal(h.compareHands(handA, handB), -1);
+
+        var handA = createHand(['4c', '4d', '3h', '3s', 'Ac'], true);
+        var handB = createHand(['4s', '4h', '3d', '3c', 'As'], true);
+        test.equal(h.compareHands(handA, handB), 0);
+
+        var handA = createHand(['Tc', 'Td', '5h', '5s', 'Ac'], true);
+        var handB = createHand(['Ts', 'Th', '5d', '5c', 'Ks'], true);
+        test.equal(h.compareHands(handA, handB), 1);
+
+        test.done()
+    },
+
+    testTrips: function(test) {
+        var handA = createHand(['Tc', 'Td', 'Th', 'As', 'Kc'], true);
+        var handB = createHand(['Js', 'Jh', 'Jd', '3c', '2s'], true);
+        test.equal(h.compareHands(handA, handB), -1);
+
+        var handA = createHand(['3c', '3d', '3h', '4s', '5c'], true);
+        var handB = createHand(['2s', '2h', '2d', 'Qc', 'Js'], true);
+        test.equal(h.compareHands(handA, handB), 1);
+
+        test.done()
+    },
+
+    testStraight: function(test) {
+        var handA = createHand(['Ac', '2d', '3h', '4s', '5c'], true);
+        var handB = createHand(['Ts', 'Jh', 'Qd', 'Kc', 'As'], true);
+        test.equal(h.compareHands(handA, handB), -1);
+
+        var handA = createHand(['6c', '7d', '8h', '9s', 'Tc'], true);
+        var handB = createHand(['6c', '7d', '8h', '9h', 'Td'], true);
+        test.equal(h.compareHands(handA, handB), 0);
+
+        test.done()
+    },
+
+    testFlush: function(test) {
+        var handA = createHand(['Ac', 'Tc', '7c', '5c', '2c'], true);
+        var handB = createHand(['Ac', 'Tc', '7c', '5c', '3c'], true);
+        test.equal(h.compareHands(handA, handB), -1);
+
+        var handA = createHand(['Ah', 'Th', '7h', '5h', '3h'], true);
+        var handB = createHand(['Ah', 'Th', '7h', '5h', '3h'], true);
+        test.equal(h.compareHands(handA, handB), 0);
+
+        var handA = createHand(['As', 'Ts', '7s', '5s', '3s'], true);
+        var handB = createHand(['As', '9s', '7s', '5s', '3s'], true);
+        test.equal(h.compareHands(handA, handB), 1);
+
+        test.done()
+    },
+
+    testFullHouse: function(test) {
+        var handA = createHand(['Ac', 'Ad', 'Ah', '2s', '2c'], true);
+        var handB = createHand(['Ac', 'Ad', 'Ah', '7c', '7s'], true);
+        test.equal(h.compareHands(handA, handB), -1);
+
+        var handA = createHand(['Ac', 'Ad', 'Ah', '2s', '2c'], true);
+        var handB = createHand(['Ks', 'Kh', 'Kd', 'Qc', 'Qs'], true);
+        test.equal(h.compareHands(handA, handB), 1);
+
+        test.done()
+    },
+
+    testQuads: function(test) {
+        var handA = createHand(['Ac', 'Ad', 'Ah', 'As', 'Kc'], true);
+        var handB = createHand(['Kc', 'Kd', 'Kh', 'Kc', 'As'], true);
+        test.equal(h.compareHands(handA, handB), 1);
+
         test.done()
     },
 };
